3e8ab7c53be4721d449c91baddd951d4dff28744
Murshida <murshidatv@gmail.com>
2024-07-18 10:46:40 +0530

    added linkedList files

--- Dsa Array/sum.js
+++ Dsa Array/sum.js
@@ -1,3 +1,27 @@
+
+/*
 const arr =[2,3,4,5,6,7];
 const sum = arr.reduce((total,num)=>total + num,0);
 console.log(sum);
+*/
+/*
+function sumArray(arr,n){
+    if(n<=0){
+        return 0;
+    }else{
+        return arr[n-1]+sumArray(arr,n-1);
+    }
+}
+let arr = [1,2,3,4,5];
+console.log(sumArray(arr,arr.length));
+*/
+
+function sumArray(arr,n){
+    if(n<=0){
+        return 1;
+    }else{
+        return arr[n-1]*sumArray(arr,n-1);
+    }
+}
+let arr = [10,10];
+console.log(sumArray(arr,arr.length));
